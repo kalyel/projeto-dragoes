005d04a4fc995367db8a7363b885184291598d64
Kalyel Mendes <kalyelmendesdapaixao@yahoo.com.br>
2024-08-11 11:14:03 -0300

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,47 @@
+const botoesCarrossel = document.querySelectorAll(".botao");
+const imagens = document.querySelectorAll(".imagem");
+const informacoes = document.querySelectorAll(".informacoes");
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desativarBotaoSelecionado();
+
+        marcarBotaoComoSelecionado(botao);
+
+        escondeImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+
+        esconderInformacoesAtivas();
+
+        MostrarInformacoes(indice);
+    });
+});
+
+function marcarBotaoComoSelecionado(botao) {
+    botao.classList.add("selecionado");
+}
+
+function MostrarInformacoes(indice) {
+    informacoes[indice].classList.add("ativa");
+}
+
+function esconderInformacoesAtivas() {
+    const informacoesAtiva = document.querySelector(".informacoes.ativa");
+    informacoesAtiva.classList.remove("ativa");
+}
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add("ativa");
+}
+
+function escondeImagemAtiva() {
+    const imagemAtiva = document.querySelector(".ativa");
+    imagemAtiva.classList.remove("ativa");
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector(".selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
